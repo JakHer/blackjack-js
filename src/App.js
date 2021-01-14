94fda2dbd66234b6eb9d32a74df955fb57351a2a
JaHe <kubahermyt@gmail.com>
2021-01-14 21:53:49 +0100

    feat: fetched data test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-const App = () => (
-  <div className="App">
-    <h1>Siema</h1>
-  </div>
-);
+const App = () => {
+  const [responseData, setResponseData] = useState(false);
+
+  const fetchDeck = async () => {
+    const result = await axios(
+      'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6',
+    );
+    setResponseData(result.data);
+  };
+
+  // useEffect(() => {
+  //   fetchDeck();
+  // }, []);
+
+  return (
+    <>
+      <button type="button" onClick={fetchDeck}>
+        Start New Game
+      </button>
+      <p>{responseData}</p>
+    </>
+  );
+};
 
 export default App;
